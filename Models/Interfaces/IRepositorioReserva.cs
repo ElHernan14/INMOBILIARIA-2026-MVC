b04baeba51d2663211514f12b9f7d333001e2944
Arancibia Leiria Leandro Nicanor <Nicanor95@users.noreply.github.com>
2026-08-27 19:05:53 -0300

    Agregar interfaz de Reserva

--- Models/Interfaces/IRepositorioReserva.cs
+++ Models/Interfaces/IRepositorioReserva.cs
@@ -1,7 +1,10 @@
 namespace INMOBILIARIA.Models.Interfaces
 {
-    public interface IRepositorioReserva : IRepositorio<Reserva>
+    public interface IRepositorioReserva
     {
-        // desarrollar con lo que falte
+        public abstract int Alta(Reserva p);
+        public abstract int Baja(int id);
+        public abstract int Modificacion(Reserva p);
+        public abstract Reserva? ObtenerPorId(int id);
     }
 }
