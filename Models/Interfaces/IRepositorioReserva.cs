8555634fbf8877fa8df289a7b8d747e1afbd7cc1
Arancibia Leiria Leandro Nicanor <Nicanor95@users.noreply.github.com>
2026-08-27 20:41:00 -0300

    Actualizar interfaz Reserva

--- Models/Interfaces/IRepositorioReserva.cs
+++ Models/Interfaces/IRepositorioReserva.cs
@@ -6,5 +6,9 @@ namespace INMOBILIARIA.Models.Interfaces
         public abstract int Baja(int id);
         public abstract int Modificacion(Reserva p);
         public abstract Reserva? ObtenerPorId(int id);
+        public abstract List<Reserva> ObtenerTodas();
+        public abstract List<Reserva> ObtenerPorInmueble(int id);
+        public abstract List<Reserva> ObtenerPorInquilino(int id);
+        public abstract List<Reserva> ObtenerPorFecha(DateOnly fecha);
     }
 }
