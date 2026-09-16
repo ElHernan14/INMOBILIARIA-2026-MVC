886786784f3edec17b9da2b728a5c630b60dae66
kevin <kevinenriquep26@gmail.com>
2026-09-16 15:58:41 -0300

    alta, baja y modificacion de inquilino con redirect y mensaje de exito y error

--- Controllers/InquilinoController.cs
+++ Controllers/InquilinoController.cs
@@ -1,6 +1,7 @@
 using INMOBILIARIA.Models.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using INMOBILIARIA.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace INMOBILIARIA.Controllers
 {
@@ -16,48 +17,122 @@ namespace INMOBILIARIA.Controllers
             this.configuration = configuration;
         }
 
+		[Authorize]
         public IActionResult Index(bool activo = true, string? nombre = null, string? apellido = null, string? dni = null, string? email = null, int page = 1)
-        {
-            int tam = 5;
-
-            List<Inquilino> lista = repositorioInquilino.ObtenerTodos(activo, nombre, apellido, dni, email, tam, Math.Max(page, 1));
-            int total = repositorioInquilino.ContarTodos(activo, nombre, apellido, dni, email);
-
-            ViewBag.activo = activo;
-            ViewBag.nombre = nombre;
-            ViewBag.apellido = apellido;
-            ViewBag.dni = dni;
-            ViewBag.email = email;
-            ViewBag.page = page;
-            ViewBag.TotalPaginas = total % tam == 0 ? total / tam : total / tam + 1;
-
-            return View(lista);
-        }
-
-        [HttpPost]
-        // [ValidateAntiForgeryToken] // quitar cuando se requiera
-        public ActionResult Create([FromBody] Inquilino inquilino)
         {
             try
             {
+                if (TempData.ContainsKey("Mensaje"))
+				    ViewBag.Mensaje = TempData["Mensaje"];
+                if (TempData.ContainsKey("Error"))
+                    ViewBag.Error = TempData["Error"];
 
-                if(inquilino == null) return BadRequest("Los datos del inquilino son nulos");
+                int tam = 5;
 
-				if (!ModelState.IsValid) return BadRequest(ModelState);
+                List<Inquilino> lista = repositorioInquilino.ObtenerTodos(activo, nombre, apellido, dni, email, tam, Math.Max(page, 1));
+                int total = repositorioInquilino.ContarTodos(activo, nombre, apellido, dni, email);
 
-                repositorioInquilino.Alta(inquilino);
+                ViewBag.activo = activo;
+                ViewBag.nombre = nombre;
+                ViewBag.apellido = apellido;
+                ViewBag.dni = dni;
+                ViewBag.email = email;
+                ViewBag.page = page;
+                ViewBag.TotalPaginas = total % tam == 0 ? total / tam : total / tam + 1;
 
-                return Ok("inquilino creado");
+                return View(lista);
             }
             catch (Exception ex)
             {
-                Console.Error.WriteLine("Ocurrió un error, en InquilinoController - create", ex);
-				return StatusCode(500, "Ocurrió un error");
+                ViewBag.Error = ex.Message;
+                Console.Error.WriteLine("Ocurrió un error, en InquilinoController - index - get", ex);
+				return View();
+            }
+
+            
+        }
+
+		[Authorize]
+        public ActionResult Create()
+		{
+			try
+			{
+                if (TempData.ContainsKey("Mensaje"))
+                    ViewBag.Mensaje = TempData["Mensaje"];
+                if (TempData.ContainsKey("Error"))
+                    ViewBag.Error = TempData["Error"];
+
+				return View();
+			}
+			catch (Exception ex)
+			{
+                TempData["Error"] = ex.Message;
+				Console.Error.WriteLine("Ocurrió un error, en InquilinoController - create - get", ex);
+				return RedirectToAction(nameof(Index));
+			}
+		}
+
+
+        [HttpPost]
+		[Authorize]
+        [ValidateAntiForgeryToken] 
+        public ActionResult Create(Inquilino inquilino)
+        {
+            try
+            {
+
+                if(inquilino == null)
+                {
+                    TempData["Error"] = "Los datos del inquilino son nulos";
+                    return RedirectToAction(nameof(Create));
+                }
+
+				if (!ModelState.IsValid) return BadRequest(ModelState);
+
+
+                repositorioInquilino.Alta(inquilino);
+
+                TempData["Mensaje"] = "El inquilino se registró correctamente.";
+				return RedirectToAction(nameof(Index));
+            }
+            catch (Exception ex)
+            {
+                TempData["Error"] = ex.Message;
+                Console.Error.WriteLine("Ocurrió un error, en InquilinoController - create - post", ex);
+				return RedirectToAction(nameof(Index));
             }
         }
 
-         [HttpPost]
-         public ActionResult Update([FromBody] Inquilino inquilino)
+        [Authorize]
+        public ActionResult Update(int id)
+		{
+			try
+			{
+                if (TempData.ContainsKey("Mensaje"))
+                    ViewBag.Mensaje = TempData["Mensaje"];
+                if (TempData.ContainsKey("Error"))
+                    ViewBag.Error = TempData["Error"];
+
+                Inquilino? inquilino = repositorioInquilino.ObtenerPorId(id);
+                if(inquilino == null)
+                {
+                    TempData["Error"] = "Inquilino no encontrado";
+                    return RedirectToAction(nameof(Index));
+                }
+
+				return View(inquilino);
+			}
+			catch (Exception ex)
+			{
+                TempData["Error"] = ex.Message;
+				Console.Error.WriteLine("Ocurrió un error, en InquilinoController - update - get", ex);
+				return RedirectToAction(nameof(Index));
+			}
+		}
+
+        [HttpPost]
+		[Authorize]
+        public ActionResult Update(Inquilino inquilino)
         {
             try
             {
@@ -65,42 +140,77 @@ namespace INMOBILIARIA.Controllers
 
 				if (!ModelState.IsValid) return BadRequest(ModelState);
 
-                Inquilino i = repositorioInquilino.ObtenerPorId(inquilino.Id);
+                Inquilino? i = repositorioInquilino.ObtenerPorId(inquilino.Id);
                 if(i == null)
                 {
-                    return NotFound("Inquilino no encontrado");
+                    TempData["Error"] = "Inquilino no encontrado";
+                    return RedirectToAction(nameof(Index));
                 }
 
                 repositorioInquilino.Modificacion(inquilino);
 
-                return Ok("Inquilino editado");
+                TempData["Mensaje"] = "El inquilino se editó correctamente.";
+				return RedirectToAction(nameof(Index));
             }
             catch (Exception ex)
             {
-                Console.Error.WriteLine("Ocurrió un error, en InquilinoController - update", ex);
-				return StatusCode(500, "Ocurrió un error");
+                TempData["Error"] = ex.Message;
+                Console.Error.WriteLine("Ocurrió un error, en InquilinoController - update - post", ex);
+                return RedirectToAction(nameof(Update));
+
             }
         }
 
-        [HttpDelete]
+        [Authorize(Policy = "ADMINISTRADOR")]
         public ActionResult Delete(int id)
+		{
+			try
+			{
+                if (TempData.ContainsKey("Mensaje"))
+                    ViewBag.Mensaje = TempData["Mensaje"];
+                if (TempData.ContainsKey("Error"))
+                    ViewBag.Error = TempData["Error"];
+
+                Inquilino? inquilino = repositorioInquilino.ObtenerPorId(id);
+                if(inquilino == null)
+                {
+                    TempData["Error"] = "Inquilino no encontrado";
+                    return RedirectToAction(nameof(Index));
+                }
+
+				return View(inquilino);
+			}
+			catch (Exception ex)
+			{
+                TempData["Error"] = ex.Message;
+				Console.Error.WriteLine("Ocurrió un error, en InquilinoController - delete - get", ex);
+				return RedirectToAction(nameof(Index));
+			}
+        }
+
+        [HttpPost]
+        [Authorize(Policy = "ADMINISTRADOR")]
+        public ActionResult Delete(int id, Inquilino inquilino)
         {
             try
             {
                 Inquilino i = repositorioInquilino.ObtenerPorId(id);
                 if(i == null)
                 {
-                    return NotFound("Inquilino no encontrado");
+                    TempData["Error"] = "Inquilino no encontrado";
+                    return RedirectToAction(nameof(Index));
                 }
 
                 repositorioInquilino.Baja(id);
 
-                return Ok("Inquilino eliminado");
+                TempData["Mensaje"] = "El inquilino se elimino correctamente.";
+				return RedirectToAction(nameof(Index));
             }
             catch (Exception ex)
             {
-                Console.Error.WriteLine("Ocurrió un error, en InquilinoController - delete", ex);
-				return StatusCode(500, "Ocurrió un error");
+                TempData["Error"] = ex.Message;
+                Console.Error.WriteLine("Ocurrió un error, en InquilinoController - delete - post", ex);
+				return RedirectToAction(nameof(Index));
             }
         }
     }
